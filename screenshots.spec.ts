bd6f30aa5ea037cfd9f6a4c7bcda558e202e8b9e
Allan Taveras <38157234+allantaveras@users.noreply.github.com>
2026-08-25 03:53:02 -0400

    Update screenshots to show populated data

--- screenshots.spec.ts
+++ screenshots.spec.ts
@@ -9,17 +9,17 @@ test('capture screenshots', async ({ page }) => {
 
   // Dashboard
   await page.goto('http://localhost:3000/');
-  await page.waitForTimeout(1500);
+  await page.waitForTimeout(3000);
   await page.screenshot({ path: 'public/docs/dashboard.png', fullPage: true });
 
   // Upload
   await page.goto('http://localhost:3000/upload');
-  await page.waitForTimeout(1500);
+  await page.waitForTimeout(3000);
   await page.screenshot({ path: 'public/docs/upload.png', fullPage: true });
 
   // Explore
   await page.goto('http://localhost:3000/explore');
-  await page.waitForTimeout(1500);
+  await page.waitForTimeout(3000);
   await page.screenshot({ path: 'public/docs/explore.png', fullPage: true });
 
   // Generate
